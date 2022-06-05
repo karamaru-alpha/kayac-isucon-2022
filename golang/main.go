35d21eeeceee77a9e04537d47c5dba2aab95c32c
karamaru <mrnk3078@gmail.com>
2022-06-05 22:08:40 +0900

    fix

--- golang/main.go
+++ golang/main.go
@@ -463,13 +463,14 @@ func getPopularPlaylistSummaries(ctx context.Context, db connOrTx, userAccount s
 
 		UserAccount     string `db:"account"`
 		UserDisplayName string `db:"display_name"`
+		UserIsBan       bool   `db:"is_ban"`
 
 		IsFavorited bool `db:"is_favorited"`
-	}, 0, 100)
+	}, 0, 125)
 	if err := db.SelectContext(
 		ctx,
 		&playlists,
-		`SELECT a.ulid, a.name, a.is_public, a.fav_count, a.song_count, a.created_at, a.updated_at, b.account, b.display_name, c.id IS NOT NULL AS is_favorited FROM playlist a JOIN user b ON a.user_account = b.account LEFT JOIN playlist_favorite c ON a.id = c.playlist_id AND c.favorite_user_account = ? WHERE a.is_public = 1 AND b.is_ban = 0 ORDER BY a.fav_count DESC LIMIT 100`,
+		`SELECT a.ulid, a.name, a.is_public, a.fav_count, a.song_count, a.created_at, a.updated_at, b.account, b.display_name, b.is_ban, c.id IS NOT NULL AS is_favorited FROM playlist a JOIN user b ON a.user_account = b.account LEFT JOIN playlist_favorite c ON a.id = c.playlist_id AND c.favorite_user_account = ? WHERE a.is_public = 1 ORDER BY a.fav_count DESC LIMIT 125`,
 		userAccount,
 	); err != nil {
 		return nil, fmt.Errorf(
@@ -483,6 +484,9 @@ func getPopularPlaylistSummaries(ctx context.Context, db connOrTx, userAccount s
 
 	result := make([]Playlist, 0, len(playlists))
 	for _, playlist := range playlists {
+		if playlist.UserIsBan {
+			continue
+		}
 		result = append(result, Playlist{
 			ULID:            playlist.ULID,
 			Name:            playlist.Name,
