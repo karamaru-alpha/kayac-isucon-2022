c0c76941da5fad6d42b7b8074cda17ef633dc7ba
karamaru <mrnk3078@gmail.com>
2022-06-05 15:11:46 +0900

    fav_playlist

--- golang/main.go
+++ golang/main.go
@@ -356,7 +356,7 @@ func getFavoritesCountByPlaylistID(ctx context.Context, db connOrTx, playlistID 
 	if err := db.GetContext(
 		ctx,
 		&count,
-		"SELECT COUNT(*) AS cnt FROM playlist_favorite where playlist_id = ?",
+		"SELECT fav_count FROM playlist where id = ?",
 		playlistID,
 	); err != nil {
 		return 0, fmt.Errorf(
